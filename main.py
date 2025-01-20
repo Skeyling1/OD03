3f6ebb6886733b433cc2b2ad110a6cbeb8b674a7
Skeyling1 <skeyling1@gmail.com>
2025-01-21 01:46:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 # Пузырьковая сортировка
 def bubble(s):
-    for run in s:
-        for i in s:
-            if i > i+1:
-                run[i], run[i+1] = run[i+1], run[i]
+    for run in range(len(s)):
+        for i in range(len(s)-1-run):
+            if s[i] > s[i+1]:
+                s[i], s[i+1] = s[i+1], s[i]
+    return s
+
+
+s = [9, 6, 4, 3, 1, 2]
+
+print(bubble(s))
+
+
+# fast sorting
+def fast_sort(s):
+    elenent = s[0]
+    left = []
+    cenrte = []
+    right = []
+   # left = lambda
